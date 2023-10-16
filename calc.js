df4a725c00abd31baf2522daa28a9ffb466e2004
SYLVIA ONCHOKA <mogotusylvia@gmail.com>
2023-10-16 14:50:46 +0300

    Add files via upload
    first commit

--- calc.js
+++ calc.js
@@ -11,13 +11,13 @@ let output3= document.getElementById("year-output");
 let bounce1= document.getElementById('error1');
 let bounce2= document.getElementById('error2');
 let bounce3= document.getElementById('error3');
-
+ let form = document.querySelector('form')
 let submit_btn = document.getElementById("btn");
 
 day.addEventListener("input", function(){
     
     if (day.value > 31  || day.value <= 0 ){
-        bounce1.textContent=" must be a valid date";
+        bounce1.textContent="not valid ";
         isValid = false;
         return;
     }else{
@@ -29,7 +29,7 @@ day.addEventListener("input", function(){
 month.addEventListener("input", function(){
     
     if (month.value > 12 || month.value <= 0){
-        bounce2.textContent=" must be a valid date";
+        bounce2.textContent=" not valid";
         isValid = false;
         return;
     }else{
@@ -40,7 +40,7 @@ month.addEventListener("input", function(){
 })
 year.addEventListener("input", function(){
     if (year.value > 2023 || year.value <= 0){
-        bounce3.textContent=" must be a valid date";
+        bounce3.textContent="not valid";
         isValid = false;
         return;
     }else{
@@ -49,6 +49,21 @@ year.addEventListener("input", function(){
     }
      
 })
-submit_btn.addEventListener("click" function(){
+// output1.addEventListener()
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    var my_date = new Date();
+    let DOB = my_date.getDay();
+    let MOB =my_date.getMonth();
+    let YOB = my_date.getFullYear();
+
+    output1.textContent = parseInt(31)- +parseInt(day.value) + "days";
+    output2.textContent= parseInt(MOB) -parseInt(month.value) + "months";
+    output3.textContent=parseInt(YOB)-parseInt(year.value) + "years";
     
+   
+
+        
 })
+ 
